ce51755316502decb82507cc70aa2f369d908a33
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-08-15 20:09:09 +0545

    router

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <a className="navbar-brand" href="#">Navbar</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -20,24 +20,25 @@ function App() {
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-       <Link className="nav-link" to="{'/Data-Binding'}">Data Binding</Link>
-      </li>
-      <li className="nav-item">
-         <Link className="nav-link" to="{'/useEffect'}">Use Effect</Link>
-      </li>
-      <li className="nav-item dropdown">
-         <Link className="nav-link" to="{'/Condition-Render'}">Condition Render</Link>
-        
-       
-      </li>
-      <li className="nav-item">
-             <Link className="nav-link" to="{'/List-Example'}">List</Link>
-      </li>
+    <li className="nav-item active"> 
+  <Link className="nav-link" to="/Data-Binding">Data Binding</Link> 
+</li> 
+
+<li className="nav-item"> 
+  <Link className="nav-link" to="/useEffect">Use Effect</Link> 
+</li> 
+
+<li className="nav-item dropdown"> 
+  <Link className="nav-link" to="/Condition-Render">Condition Render</Link> 
+</li> 
+
+<li className="nav-item"> 
+  <Link className="nav-link" to="/List-Example">List</Link> 
+</li>
     </ul>
    
   </div>
-</nav> */}
+</nav>
 
 
 
