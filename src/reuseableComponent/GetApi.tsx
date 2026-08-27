2b0cf055075028bd303824460d8aeaf86399e0be
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-08-27 21:16:27 +0545

    api integration

--- src/reuseableComponent/GetApi.tsx
+++ src/reuseableComponent/GetApi.tsx
@@ -5,12 +5,18 @@ function GetApi(){
   const [userList,setUserList]=useState<UserModel[]>([]);
 
 
+  const [vendorList,setVendorList]=useState<VendorModel[]>([]);
+
   useEffect(()=>{
+    debugger
     getAllUsers();
+
+    getAllVendors();
 
   },[])
 
   const getAllUsers= async()=>{
+    debugger
    
     const response=await fetch("https://jsonplaceholder.typicode.com/users");
     const userList=await response.json();
@@ -18,6 +24,19 @@ function GetApi(){
 
 
 
+
+
+
+  }
+
+
+  
+
+
+  const getAllVendors=async()=>{
+    const response =await fetch("https://api.freeprojectapi.com/api/BusBooking/GetBusVendors");
+    const vendorList=await response.json();
+    setVendorList(vendorList);
   }
 
  return<div>
@@ -29,6 +48,28 @@ function GetApi(){
           return<li>{user.name}-{user.email}</li>})
         }
       </ul>
+    </div>
+    <div className="col-6">
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Vendor Name</th>
+            <th>Contact No</th>
+            <th>Email Id</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+vendorList.map((vendor:VendorModel)=>{
+  return<tr>
+    <td>{vendor.vendorName}</td>
+    <td>{vendor.contactNo}</td>
+    <td>{vendor.emailId}</td>
+  </tr>
+})
+          }
+        </tbody>
+      </table>
     </div>
   </div>
 
@@ -46,4 +87,11 @@ export default GetApi;
   phone: string
   website: string
   company: any
+}
+
+export interface VendorModel {
+
+  vendorName: string,
+    contactNo: string,
+    emailId: string
 }
